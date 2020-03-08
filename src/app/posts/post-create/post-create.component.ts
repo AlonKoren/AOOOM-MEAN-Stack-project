52c9e60b56c20bfa34bec15c15a77f4b07b24bc4
Alon Koren <alonkoren312@gmail.com>
2020-03-08 21:26:11 +0200

    Add Image Validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,8 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {PostService} from '../post.service';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {Post} from '../post.model';
+import {mimeType} from './mime-type.validator';
+
 
 @Component({
   selector: 'app-post-create',
@@ -30,7 +32,8 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required]
       }),
       image: new FormControl(null, {
-        validators: [Validators.required]
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
       }),
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
